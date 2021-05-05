10af0088f31954095cba4625d55d0cd39cf26fb4
Sviatoslav <cdzjckfd>
2021-05-05 14:11:18 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -49,50 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
     initMessaging();
     super.initState();
   }
-  void getToken() async {
-    print(await messaging.getToken());
-  }
-  @override
-  Widget build(BuildContext context) {
-    getToken();
-    return Scaffold(
-
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-
-          ],
-        ),
-      ),
-     // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-
-  void initMessaging() {
-    var androiInit = AndroidInitializationSettings('ic_launcher');
-
-    var iosInit = IOSInitializationSettings();
-
-    var initSetting = InitializationSettings(android: androiInit, iOS: iosInit);
-
-    fltNotification = FlutterLocalNotificationsPlugin();
-
-    fltNotification.initialize(initSetting);
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      showNotification();
-    });
-  }
-
-  void showNotification() async {
+  tion() async {
     var androidDetails =
     AndroidNotificationDetails('1', 'channelName', 'channel Description');
 
